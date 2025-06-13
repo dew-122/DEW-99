1c6c1c9aa272d99fc402866d26f15cf257fbe909
dew-122 <revdeepalokat@gmail.com>
2025-06-13 10:07:11 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*DEW-99 [The Powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is your Session ID — copy this ID and paste it into your config.js file.*\n\n🔰 *Developer:* Dew-99 Developer (Imopjith Approved ✅)\n📧 *Contact:* denethdewidu26@gmail.com\n\n💬 *Ask me anything here:* wa.me/message/WKGLBR2PCETWD1\n\n📲 *Join my WhatsApp Group:*\nhttps://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ\n\n🌐 *Join My DARK WEB Server:*\nhttps://darkweb.link/your-darkweb-link`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://imgur.com/a/9PtHy70.jpg",
               },
               caption: sid,
             });
